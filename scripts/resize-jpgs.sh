b1ca1ee6399bd6f27328f4b83f8a7dc92bab1602
luddwichr <luddwichr@users.noreply.github.com>
2022-09-01 23:21:21 +0200

    increase quality of small images

--- scripts/resize-jpgs.sh
+++ scripts/resize-jpgs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sourcePath="$HOME/Downloads/sophiebauhaus/*.jpg"
-magick mogrify -resize 300x -quality 70 -path ../images/small "$sourcePath"
+magick mogrify -resize 300x -quality 90 -path ../images/small "$sourcePath"
 magick mogrify -resize 500x -quality 70 -path ../images/medium "$sourcePath"
 magick mogrify -resize 1500x -quality 70 -path ../images/large "$sourcePath"
