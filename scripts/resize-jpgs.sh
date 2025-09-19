8ece80d8ac732a615ae76e57e0c89391de0aebdd
luddwichr <luddwichr@users.noreply.github.com>
2025-09-19 10:08:49 +0200

    adjust resize-jpgs.sh

--- scripts/resize-jpgs.sh
+++ scripts/resize-jpgs.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 sourcePath="$HOME/Downloads/sophiebauhaus/*.jpg"
 
-# note: install magick via 
-# magick mogrify -resize 300x -quality 90 -path ../images/small "$sourcePath"
-#magick mogrify -resize 500x -quality 70 -path ../images/medium "$sourcePath"
-#magick mogrify -resize 2000x -quality 80 -path ../images/tests/high-res "$sourcePath"
+echo "converting the following files"
+echo $sourcePath
 
-# cwebp docu: https://developers.google.com/speed/webp/docs/cwebp?hl=de
+# cwebp documentation: https://developers.google.com/speed/webp/docs/cwebp?hl=de
+# install cwebp via `sudo apt install webp`
 for file in $sourcePath
 do
   filename="${file##*/}"
